ab512bd4a4138a4c2291958fb92ca22a612c8922
Yuki <134689144+yuki10000@users.noreply.github.com>
2024-09-24 01:48:07 +0900

    #15 植田くん作業分　imgの読み込み改善

--- webapp/backend/src/domains/auth_service.rs
+++ webapp/backend/src/domains/auth_service.rs
@@ -1,15 +1,11 @@
-use std::path::{Path, PathBuf};
-use std::process::Command;
-
-use actix_web::web::Bytes;
-use log::error;
-
+use super::dto::auth::LoginResponseDto;
 use crate::errors::AppError;
 use crate::models::user::{Dispatcher, Session, User};
 use crate::utils::{generate_session_token, hash_password, verify_password};
-
-use super::dto::auth::LoginResponseDto;
-
+use actix_web::web::Bytes;
+use log::error;
+use std::path::{Path, PathBuf};
+use tokio::process::Command; // tokioのCommandをインポート
 pub trait AuthRepository {
     async fn create_user(&self, username: &str, password: &str, role: &str)
         -> Result<(), AppError>;
@@ -30,17 +26,28 @@ pub trait AuthRepository {
     async fn find_session_by_session_token(&self, session_token: &str)
         -> Result<Session, AppError>;
 }
-
 #[derive(Debug)]
 pub struct AuthService<T: AuthRepository + std::fmt::Debug> {
     repository: T,
 }
-
 impl<T: AuthRepository + std::fmt::Debug> AuthService<T> {
     pub fn new(repository: T) -> Self {
         AuthService { repository }
     }
-
+    fn create_login_response(
+        user: &User,
+        session_token: String,
+        dispatcher: Option<&Dispatcher>, // 参照を取る
+    ) -> LoginResponseDto {
+        LoginResponseDto {
+            user_id: user.id,
+            username: user.username.clone(),
+            session_token,
+            role: user.role.clone(),
+            dispatcher_id: dispatcher.map(|d| d.id),
+            area_id: dispatcher.map(|d| d.area_id),
+        }
+    }
     pub async fn register_user(
         &self,
         username: &str,
@@ -51,156 +58,131 @@ impl<T: AuthRepository + std::fmt::Debug> AuthService<T> {
         if role == "dispatcher" && area.is_none() {
             return Err(AppError::BadRequest);
         }
-
-        if (self.repository.find_user_by_username(username).await?).is_some() {
+        if self
+            .repository
+            .find_user_by_username(username)
+            .await?
+            .is_some()
+        {
             return Err(AppError::Conflict);
         }
-
-        let hashed_password = hash_password(password).unwrap();
-
+        let hashed_password = hash_password(password).map_err(|_| AppError::InternalServerError)?;
         self.repository
             .create_user(username, &hashed_password, role)
             .await?;
-
         let session_token = generate_session_token();
-
-        match self.repository.find_user_by_username(username).await? {
-            Some(user) => {
-                self.repository
-                    .create_session(user.id, &session_token)
-                    .await?;
-                match user.role.as_str() {
-                    "dispatcher" => {
-                        self.repository
-                            .create_dispatcher(user.id, area.unwrap())
-                            .await?;
-                        let dispatcher = self
-                            .repository
-                            .find_dispatcher_by_user_id(user.id)
-                            .await?
-                            .unwrap();
-                        Ok(LoginResponseDto {
-                            user_id: user.id,
-                            username: user.username,
-                            session_token,
-                            role: user.role,
-                            dispatcher_id: Some(dispatcher.id),
-                            area_id: Some(dispatcher.area_id),
-                        })
-                    }
-                    _ => Ok(LoginResponseDto {
-                        user_id: user.id,
-                        username: user.username,
-                        session_token,
-                        role: user.role,
-                        dispatcher_id: None,
-                        area_id: None,
-                    }),
-                }
-            }
-            None => Err(AppError::InternalServerError),
+        let user = self
+            .repository
+            .find_user_by_username(username)
+            .await?
+            .ok_or(AppError::InternalServerError)?;
+        self.repository
+            .create_session(user.id, &session_token)
+            .await?;
+        if user.role == "dispatcher" {
+            let area_id = area.unwrap();
+            self.repository.create_dispatcher(user.id, area_id).await?;
+            let dispatcher = self
+                .repository
+                .find_dispatcher_by_user_id(user.id)
+                .await?
+                .ok_or(AppError::InternalServerError)?;
+            Ok(Self::create_login_response(
+                &user,
+                session_token,
+                Some(&dispatcher),
+            )) // 参照を渡す
+        } else {
+            Ok(Self::create_login_response(&user, session_token, None))
         }
     }
-
     pub async fn login_user(
         &self,
         username: &str,
         password: &str,
     ) -> Result<LoginResponseDto, AppError> {
-        if let Some(user) = self.repository.find_user_by_username(username).await? {
-            // パスワードの検証
-            if !verify_password(&user.password, password).unwrap() {
-                return Err(AppError::Unauthorized);
-            }
-
-            // セッショントークンの生成
-            let session_token = generate_session_token();
-            self.repository
-                .create_session(user.id, &session_token)
-                .await?;
-
-            // ディスパッチャーIDとエリアIDの初期化
-            let mut dispatcher_id = None;
-            let mut area_id = None;
-
-            // ユーザーのロールが "dispatcher" の場合
-            if user.role == "dispatcher" {
-                if let Some(dispatcher) =
-                    self.repository.find_dispatcher_by_user_id(user.id).await?
-                {
-                    dispatcher_id = Some(dispatcher.id);
-                    area_id = Some(dispatcher.area_id);
-                } else {
-                    return Err(AppError::InternalServerError);
-                }
-            }
-
-            // 最終的なログインレスポンスを作成して返す
-            Ok(LoginResponseDto {
-                user_id: user.id,
-                username: user.username,
-                session_token,
-                role: user.role.clone(),
-                dispatcher_id,
-                area_id,
-            })
-        } else {
-            Err(AppError::Unauthorized)
+        let user = self
+            .repository
+            .find_user_by_username(username)
+            .await?
+            .ok_or(AppError::Unauthorized)?;
+        if !verify_password(&user.password, password).unwrap() {
+            return Err(AppError::Unauthorized);
         }
+        let session_token = generate_session_token();
+        let create_session_task = self.repository.create_session(user.id, &session_token);
+        let (dispatcher_id, area_id) = if user.role == "dispatcher" {
+            let dispatcher = self
+                .repository
+                .find_dispatcher_by_user_id(user.id)
+                .await?
+                .ok_or(AppError::InternalServerError)?;
+            (Some(dispatcher.id), Some(dispatcher.area_id))
+        } else {
+            (None, None)
+        };
+        create_session_task.await?;
+        Ok(LoginResponseDto {
+            user_id: user.id,
+            username: user.username.clone(),
+            session_token,
+            role: user.role.clone(),
+            dispatcher_id,
+            area_id,
+        })
     }
     pub async fn logout_user(&self, session_token: &str) -> Result<(), AppError> {
         self.repository.delete_session(session_token).await?;
         Ok(())
     }
-
     pub async fn get_resized_profile_image_byte(
         &self,
         user_id: i32,
         width: i32,
         height: i32,
     ) -> Result<Bytes, AppError> {
-        let profile_image_name = match self
+        let profile_image_name = self
             .repository
             .find_profile_image_name_by_user_id(user_id)
-            .await
-        {
-            Ok(Some(name)) => name,
-            Ok(None) => return Err(AppError::NotFound),
-            Err(_) => return Err(AppError::NotFound),
-        };
-
+            .await?
+            .ok_or(AppError::NotFound)?;
         let path: PathBuf =
             Path::new(&format!("images/user_profile/{}", profile_image_name)).to_path_buf();
-
-        let output = Command::new("convert")
-            .arg(&path)
-            .arg("-resize")
-            .arg(format!("{}x{}!", width, height))
-            .arg("png:-")
-            .output()
-            .map_err(|e| {
-                error!("画像リサイズのコマンド実行に失敗しました: {:?}", e);
-                AppError::InternalServerError
-            })?;
-
-        match output.status.success() {
-            true => Ok(Bytes::from(output.stdout)),
-            false => {
-                error!(
-                    "画像リサイズのコマンド実行に失敗しました: {:?}",
-                    String::from_utf8_lossy(&output.stderr)
-                );
-                Err(AppError::InternalServerError)
-            }
+        // 非同期タスクを生成
+        let resize_task = tokio::spawn(async move {
+            Command::new("convert")
+                .arg(&path)
+                .arg("-resize")
+                .arg(format!("{}x{}!", width, height))
+                .arg("png:-")
+                .output()
+                .await
+        });
+        // タスクを待機して結果を取得
+        let output = resize_task
+            .await
+            .map_err(|_| AppError::InternalServerError)?;
+        // spawnでのエラーを処理
+        let output = output.map_err(|e| {
+            error!("コマンド実行中にエラーが発生しました: {:?}", e);
+            AppError::InternalServerError
+        })?;
+        // 結果の処理
+        if output.status.success() {
+            Ok(Bytes::from(output.stdout))
+        } else {
+            error!(
+                "画像リサイズのコマンド実行に失敗しました: {:?}",
+                String::from_utf8_lossy(&output.stderr)
+            );
+            Err(AppError::InternalServerError)
         }
     }
-
     pub async fn validate_session(&self, session_token: &str) -> Result<bool, AppError> {
         let session = self
             .repository
             .find_session_by_session_token(session_token)
             .await?;
-
         Ok(session.is_valid)
     }
 }
